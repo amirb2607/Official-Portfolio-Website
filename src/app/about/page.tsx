6f109d8b47bdbc8f04f6e01b714e67b9aba75136
amirb2607 <amir@Amirs-MacBook-Pro.local>
2025-08-17 03:47:41 -0400

    added minor changes to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,20 +17,33 @@ export async function generateMetadata() {
 
 export default function About() {
   return (
-    <>
-      <Column fillWidth fillHeight padding="xl" gap="l" center>
-        <Card padding="l" maxWidth="m">
-          <Row gap="l" vertical="center">
-            <Avatar value="A" size="xl" />
-            <Column gap="xs">
-              <Text variant="display-strong-m">About Me</Text>
-              <Text onBackground="neutral-medium" wrap="balance">
-                A starter layout for the about page.
-              </Text>
-            </Column>
+    <Column fillWidth fillHeight padding="xl" gap="16" center>
+      <Row fillWidth fitHeight gap="16" s={{direction: "column"}}>
+        <Column fillWidth center>
+          <Card padding="l" maxWidth="xs">
+            <Row gap="l" vertical="center">
+              <Avatar value="A" size="xl" />
+              <Column gap="xs">
+                <Text variant="display-strong-m">Amir Badrudeen</Text>
+                <Text variant="label-default-m">America/NYC</Text>
+                <Text onBackground="neutral-medium" wrap="balance">
+                  Passionate about building exciting UI/UX interfaces and enhancing user experiences. I love creating intuitive designs that make technology accessible to everyone.
+                  With a keen eye for detail and a commitment to quality, I strive to deliver exceptional results in every project I undertake.
+                  When I'm not coding, you can find me exploring new homelab services or playing video games.
+                </Text>
+              </Column>
+            </Row>
+          </Card>
+        </Column>
+        <Column fill gap="16">
+          <Row fill center>
+            <Text variant="display-strong-m">Education</Text>
           </Row>
-        </Card>
-      </Column>
-    </>
+          <Row fill center>
+            <Text variant="display-strong-m">Work Experience</Text>
+          </Row>
+        </Column>
+      </Row>
+    </Column>
   );
 }
