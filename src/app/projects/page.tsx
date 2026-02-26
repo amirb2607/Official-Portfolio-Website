c45912170f5533f0ab0abc5d8dc825deaa1fe847
Amir B <me@amirb.dev>
2026-02-26 12:58:44 -0500

    started working on projects not up for prod

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,8 @@
 import { Column, Text } from "@/once-ui/components";
 import { Meta } from "@/once-ui/modules";
 import { meta, baseURL } from "../resources/once-ui.config";
+import DelayRepeatLetterFX from "../components/DelayRepeatLetterFX";
+import { Carousel } from "@once-ui-system/core";
 
 export async function generateMetadata() {
   return Meta.generate({
@@ -18,9 +20,27 @@ export async function generateMetadata() {
 export default function Projects() {
   return (
     <>
-      <Column fillWidth fillHeight center>
-        <Text> On The Projects Page! </Text>
-      </Column>
+    <Column padding="l">
+      <DelayRepeatLetterFX
+              text="My Projects!"
+              delay={10_000}
+              speed="slow"
+              className="font-display font-m font-strong"
+      />
+      <Carousel
+        controls={false}
+        indicator={false}
+        aspectRatio="4 / 3"
+        play={{auto: true, interval: 5000, controls: true, progress: true}}
+        items={[
+          { slide: "/images/apps/adguard/home.png", alt: "Image 1" },
+          { slide: "/images/apps/ai/home.png", alt: "Image 2" },
+        ]}
+      />
+    </Column>
+    <Column fillWidth fillHeight center>
+      <Text> On The Projects Page! </Text>
+    </Column>
     </>
   );
 }
